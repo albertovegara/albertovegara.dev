b9807f14f004e5ea9797869903124f4ce22ff8d5
alberto-vb <vegara.belmonte.alberto@gmail.com>
2025-02-10 11:18:14 +0100

    💚 fix deployment by replacing <a> with Next <Link /> component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const socialLinks = [
@@ -16,12 +17,12 @@ const socialLinks = [
 export default function Header() {
   return (
     <nav className="container sticky top-0 z-50 mb-1 flex w-full max-w-4xl items-center justify-between bg-neutral-100/80 py-4 backdrop-blur-md dark:bg-neutral-900/80">
-      <a
+      <Link
         href="/"
         className="text-md flex items-center space-x-2 font-medium tracking-tight opacity-80 transition-opacity duration-150 hover:opacity-100"
       >
         <span>alberto-vb.dev</span>
-      </a>
+      </Link>
       <div className="flex items-center space-x-1">
         {socialLinks.map((link) => {
           return link.name;
