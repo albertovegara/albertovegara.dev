d2a89879af3a446c6929aa1fbaf18f998cbc848e
alberto-vb <vegara.belmonte.alberto@gmail.com>
2025-02-12 20:22:18 +0100

    📱 make navbar responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,34 +22,38 @@ const socialLinks = [
 
 export default function Header() {
   return (
-    <nav className="container sticky top-0 z-50 mb-1 flex w-full max-w-4xl items-center justify-between bg-neutral-100/80 py-4 backdrop-blur-md dark:bg-neutral-900/80">
-      <Link
-        href="/"
-        className="text-md flex items-center space-x-2 font-medium tracking-tight opacity-80 transition-opacity duration-150 hover:opacity-100"
-      >
-        <span>alberto-vb.dev</span>
-      </Link>
-      <div className="flex items-center space-x-1">
-        <div className="min-w-[100px] grow font-mono mr-px">
-          vegara.belmonte.alberto@gmail.com
+    <nav className="container sticky top-0 z-50 mb-1 w-full max-w-4xl bg-neutral-100/80 py-4 backdrop-blur-md dark:bg-neutral-900/80">
+      <div className="grid grid-cols-1 space-y-1 md:flex md:items-center md:justify-between">
+        <Link
+          href="/"
+          className="text-md flex items-center space-x-2 font-medium tracking-tight opacity-80 transition-opacity duration-150 hover:opacity-100"
+        >
+          <span>alberto-vb.dev</span>
+        </Link>
+        <div className="grid grid-cols-1 space-y-1 md:flex md:items-center md:space-x-1">
+          <div className="min-w-[100px] grow font-mono mr-[2px]">
+            vegara.belmonte.alberto@gmail.com
+          </div>
+          <div className="flex items-center space-x-1">
+            {socialLinks.map((link) => {
+              // Destructure the icon component from the link object
+              const IconComponent = link.icon;
+              return (
+                <Link
+                  key={link.name}
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button variant="ghost" size="icon">
+                    <IconComponent className="inline-block" />
+                  </Button>
+                </Link>
+              );
+            })}
+            <ThemeToggle />
+          </div>
         </div>
-        {socialLinks.map((link) => {
-          // Destructure the icon component from the link object
-          const IconComponent = link.icon;
-          return (
-            <Link
-              key={link.name}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button variant="ghost" size="icon">
-                <IconComponent className="inline-block" />
-              </Button>
-            </Link>
-          );
-        })}
-        <ThemeToggle />
       </div>
     </nav>
   );
