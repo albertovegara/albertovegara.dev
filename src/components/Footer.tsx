475cd634df7861cabdb6afae206d075dbdfac291
albertovegara <vegara.belmonte.alberto@gmail.com>
2026-04-14 15:43:16 +0200

    Remove second last name from Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className='container mt-10 flex max-w-4xl mx-auto px-4 py-4 md:py-6 flex-col items-center justify-center space-y-1 md:flex-row md:justify-between md:space-y-0'>
       <p className='font-serif'>🧱 Innovating one line at a time</p>
       <div className='flex items-center space-x-1 text-sm md:text-md'>
-        <h2>Alberto Vegara Belmonte</h2>
+        <h2>Alberto Vegara</h2>
         <span className='text-gray-500 dark:text-gray-400'>-</span>
         <p>{currentYear}</p>
       </div>
